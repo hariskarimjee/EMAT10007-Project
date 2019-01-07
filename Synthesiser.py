8edb9ad9b1a3b2725bc56110836a320cf45db9d9
Haris <haris.karimjee@gmail.com>
2019-01-07 13:47:45 +0000

    successful modulation added needs many a bug fixed

--- Synthesiser.py
+++ Synthesiser.py
@@ -4,12 +4,13 @@ from scipy.io import wavfile
 import numpy as np
 import pyaudio, sys, threading, math, time
 import tkinter as tk
+from tkinter import messagebox
 
 RATE = 44100
 CHUNK = 2048
 
 con = 0
-
+# mod_index = int()
 
 
 
@@ -65,33 +66,28 @@ class Synthesiser(tk.Tk):
     """
     def __init__(self):
         super().__init__()
-        self.harmonic_list = []
-        self.harmonic_number = len(self.harmonic_list)
+        self.op_list = []
         self.shape_list = [("sine", "sin", 0), ("square", "sqr", 1), ("sawtooth", "saw", 2), ("triangular", "tri", 3)]
         self.wave_shape = tk.StringVar()
         self.wave_shape.set("sin")
-        # Create sound, wave control buttons
+        # Create sound control buttons
         self.start_button = tk.Button(self, text="Play sound", padx=5, pady=5, command=(lambda: self.start_sound()))
         self.start_button.grid(row=0, column=0)
-
         self.stop_button = tk.Button(self, text="Stop sound", padx=5, pady=5, command=(lambda: self.stop_sound()))
         self.stop_button.grid(row=1, column=0)
-
+        # Create plot wave button
         self.plot_button = tk.Button(self, text="Plot wave", padx=5, pady=5, command=(lambda: self.plot_wave()))
         self.plot_button.grid(row=2, column=0)
+        # Wave modulation section
 
-        self.add_wave_button = tk.Button(self, text="Add harmonic", padx=5, pady=5,
-                                         command=(lambda: self.add_harmonic()))
-        self.add_wave_button.grid(row=0, column=1)
-
-        self.remove_wave_button = tk.Button(self, text="Remove wave", padx=5, pady=5,
-                                            command = lambda: self.remove_wave())
-        self.remove_wave_button.grid(row=1, column=1)
-
-        self.clear_button = tk.Button(self, text="Clear waves", padx=5, pady=5, command=(lambda: self.clear_waves()))
-        self.clear_button.grid(row=2, column=1)
-
-        # Create Radiobuttons for selecting wave shape
+        self.add_op_button = tk.Button(self, text="Add operator", padx=5, pady=5, command=(lambda: self.add_op(self.frequency_slider.get(), self.volume_slider.get(), self.wave_shape.get(), phase=0)))
+        self.add_op_button.grid(row=0, column=8)
+        self.remove_op_button = tk.Button(self, text="Remove operator", padx=5, pady=5, command=(lambda: self.remove_op(self.op_listbox.curselection()[0])))
+        self.remove_op_button.grid(row=1, column=8)
+        self.modulate_op_button = tk.Button(self, text="Modulate operator", padx=5, pady=5, command=(lambda: self.modulate_op(self.op_list[self.op_listbox.curselection()[0]])))
+        self.modulate_op_button.grid(row=2, column=8)
+        self.op_listbox = tk.Listbox(self)
+        self.op_listbox.grid(row=0, column=9, rowspan=7, columnspan=2)
 
         for shape, shp, row in self.shape_list:
             tk.Radiobutton(self, text=shape, variable=self.wave_shape, value=shp).grid(row=row, column=2)
@@ -110,11 +106,11 @@ class Synthesiser(tk.Tk):
         self.worker = worker
 
     def start_sound(self):
-        if len(self.harmonic_list) == 0:
-            self.harmonic_list.append(Wave(self.frequency_slider.get(), self.volume_slider.get(), self.wave_shape.get(), 0))
-        if not hasattr(self, "worker") and len(self.harmonic_list) > 0:
+        if not hasattr(self, "worker") and self.op_listbox.curselection():
             self.setup_worker()
             self.worker.start()
+        elif not self.op_listbox.curselection():
+            messagebox.showinfo("Error", "Please select an operator to play")
 
     def stop_sound(self):
         if hasattr(self, "worker"):
@@ -128,70 +124,107 @@ class Synthesiser(tk.Tk):
         else:
             sys.exit()
 
-    def add_harmonic(self):
-        volume = 1/2**self.harmonic_number
-        print(volume)
-        self.harmonic_list.append(Wave((2 ** self.harmonic_number) * self.frequency_slider.get(),
-                                       volume * self.volume_slider.get(),
-                                       self.wave_shape.get(),
-                                       0))
-        self.harmonic_number += 1
+    def add_op(self, frequency, volume, shape, phase):
+        self.op_list.append(Operator(frequency, volume, shape, phase))
+        self.op_listbox.insert(tk.END, ("Freq=%sHz" % frequency, "Shape=%s" % shape))
 
-    def remove_wave(self):
-        del self.harmonic_list[self.harmonic_number]
-        self.harmonic_number -= 1
+    def remove_op(self, op_index):
+        del self.op_list[op_index]
+        self.op_listbox.delete(op_index)
 
-    def clear_waves(self):
-        self.stop_sound()
-        self.harmonic_list = []
-        self.harmonic_number = 0
+    def modulate_op(self, carrier):
+        self.mod_index = int()
+        self.mod_amp = int()
+        mod = ModulatorSelect(self)
+        root.wait_window(mod)
+        carrier.modulator = self.op_list[self.mod_index]
+        carrier.mod_amp = self.mod_amp
 
     def output_waveform(self):
-        outwave = self.harmonic_list[0].waveform()
-        if len(self.harmonic_list) > 1:
-            for wave in self.harmonic_list[1:]:
-                outwave = np.add(wave.waveform(), outwave)
-        return (1/len(self.harmonic_list)) * outwave
+        outwave = self.op_list[self.op_listbox.curselection()[0]].waveform()
+        return outwave
 
     def plot_wave(self):
-        plt.plot(self.output_waveform())
+        plt.plot(np.concatenate((self.output_waveform(), self.output_waveform())))
         plt.show()
 
 
-class Wave():
-    def __init__(self, frequency, volume, shape, phase):
+class Operator:
+    """
+    Stores information required to generate smooth wave
+
+    :method waveform: generates waveform based on class attributes
+    """
+    def __init__(self, frequency, volume, shape, phase=0, modulator=np.zeros(CHUNK)):
         self.frequency = frequency
         self.volume = volume
         self.shape = shape
         self.phase = phase
+        self.t = np.linspace(0, CHUNK / RATE, int(CHUNK))
+        self.modulator = modulator
+        self.mod_amp = 1
 
     def waveform(self):
         """
-        Generates wave as data array - frequency of datapoints dependent on rate (quality of waveform)
-        :param frequency: Frequency of wave
-        :param waveshape: Shape of wave (square, sine, sawtooth)
-        :return: Returns array containing wave data
+        Generates wave as ndarray - frequency of datapoints dependent on rate
+
+        :return: Returns ndarray containing wave data
         """
-        ti = CHUNK / RATE
-
-        t = np.linspace(0, ti, int(ti * RATE))
         f = self.frequency
+        if type(self.modulator) == np.ndarray:
+            if self.shape == "sin":
+                sig = self.volume * (np.sin(2 * np.pi * (f * self.t + self.phase)))
+                self.phase = math.modf(f * CHUNK / RATE + self.phase)[0]
+            elif self.shape == "sqr":
+                sig = self.volume * np.array(signal.square(2 * np.pi * (f * self.t + self.phase)), dtype=np.float32)
+                self.phase = math.modf(f * CHUNK / RATE + self.phase)[0]
+            elif self.shape == "saw":
+                sig = self.volume * np.array(signal.sawtooth(2 * np.pi * (f * self.t + self.phase)), dtype=np.float32)
+                self.phase = math.modf(f * CHUNK / RATE + self.phase)[0]
+            elif self.shape == "tri":
+                sig = self.volume * np.array(signal.sawtooth(2 * np.pi * (f * self.t + self.phase), width=0.5), dtype=np.float32)
+                self.phase = math.modf(f * CHUNK / RATE + self.phase)[0]
+            else:
+                sig = np.zeros(CHUNK)
+            return sig
+        elif type(self.modulator) == Operator:
+            if self.shape == "sin":
+                sig = self.volume * (np.sin(2 * np.pi * (f * self.t + self.phase) + self.mod_amp * self.modulator.waveform()))
+                self.phase = math.modf(f * CHUNK / RATE + self.phase)[0]
+            elif self.shape == "sqr":
+                sig = self.volume * np.array(signal.square(2 * np.pi * (f * self.t + self.phase) + self.mod_amp * self.modulator.waveform()), dtype=np.float32)
+                self.phase = math.modf(f * CHUNK / RATE + self.phase)[0]
+            elif self.shape == "saw":
+                sig = self.volume * np.array(signal.sawtooth(2 * np.pi * (f * self.t + self.phase) + self.mod_amp * self.modulator.waveform()), dtype=np.float32)
+                self.phase = math.modf(f * CHUNK / RATE + self.phase)[0]
+            elif self.shape == "tri":
+                sig = self.volume * np.array(signal.sawtooth(2 * np.pi * (f * self.t + self.phase) + self.mod_amp * self.modulator.waveform(), width=0.5), dtype=np.float32)
+                self.phase = math.modf(f * CHUNK / RATE + self.phase)[0]
+            else:
+                sig = np.zeros(CHUNK)
+            return sig
 
-        if self.shape == "sin":
-            sig = self.volume * (np.sin(2 * np.pi * (f * t + self.phase)))
-            self.phase = math.modf(f * CHUNK / RATE + self.phase)[0]
-        elif self.shape == "sqr":
-            sig = self.volume * np.array(signal.square(2 * np.pi * (f * t + self.phase)), dtype=np.float32)
-            self.phase = math.modf(f * CHUNK / RATE + self.phase)[0]
-        elif self.shape == "saw":
-            sig = self.volume * np.array(signal.sawtooth(2 * np.pi * (f * t + self.phase)), dtype=np.float32)
-            self.phase = math.modf(f * CHUNK / RATE + self.phase)[0]
-        elif self.shape == "tri":
-            sig = self.volume * np.array(signal.sawtooth(2 * np.pi * (f * t + self.phase), width=0.5), dtype=np.float32)
-            self.phase = math.modf(f * CHUNK / RATE + self.phase)[0]
-        return sig
 
+class ModulatorSelect(tk.Toplevel):
+    def __init__(self, master):
+        super().__init__()
+        self.master = master
 
+        self.op_box = tk.Listbox(self)
+        self.op_box.grid(row=0, column=0)
+        for op in self.master.op_list:
+            self.op_box.insert(tk.END, ("Freq=%sHz" % op.frequency, "Shape=%s" % op.shape))
+
+        self.ok_button = tk.Button(self, text="OK", padx=5, pady=5, command=(lambda: self.exit()))
+        self.ok_button.grid(row=1, column=0, rowspan=2)
+
+        self.amp_slider = tk.Scale(self, from_=20, to_=1, label="Modulation amplitude", orient=tk.VERTICAL, length=200)
+        self.amp_slider.grid(row=0,column=2)
+
+    def exit(self):
+        self.master.mod_index = self.op_box.curselection()[0]
+        self.master.mod_amp = self.amp_slider.get()
+        self.destroy()
 
 
 root = Synthesiser()
