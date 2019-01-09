be13c7baa720df53bac935a3b589cb07b8d1a5ca
Haris <haris.karimjee@gmail.com>
2019-01-09 16:07:51 +0000

    Working program with all features Stability issues present, no idea why

--- Synthesiser.py
+++ Synthesiser.py
@@ -4,15 +4,17 @@ from scipy.io import wavfile
 import numpy as np
 import pyaudio, sys, threading, math, time
 import tkinter as tk
-from tkinter import messagebox
+
 
 RATE = 44100
-CHUNK = 2048
-
-con = 0
-# mod_index = int()
-
-
+CHUNK = 1024
+NOTES = {'a': 261.6, 'w': 277, 's': 293.66, 'e': 311, 'd': 329.63, 'f': 349.23, 't':370, 'g': 392, 'y': 415, 'h': 440,
+         'u': 466, 'j': 493.88, 'k': 523.85, 'o': 554, 'l': 587.33, 'p': 622, ';': 659.26}
+"""
+:int RATE: playback sample rate
+:int CHUNK: playback frame size, containing CHUNK samples
+:dict NOTES: corresponding frequencies for piano style keyboard playback
+"""
 
 class LoopWave(threading.Thread):
     """
@@ -20,10 +22,15 @@ class LoopWave(threading.Thread):
 
     :method run: Loop to playback audio in seperate thread
     """
-    def __init__(self, master):
+    def __init__(self, master, frequency):
+        """
+        Initialise class
+        :param master: Master tk window
+        :param frequency: frequency of playback
+        """
         super().__init__()
         self.master = master
-
+        self.frequency = frequency
         self.force_quit = False
         self.end_now = False
 
@@ -34,9 +41,18 @@ class LoopWave(threading.Thread):
         p = pyaudio.PyAudio()
 
         def callback(in_data, frame_count, time_info, status):
-            data = np.array(self.master.output_waveform()).astype(np.float32).tostring()
+            """
+
+            :param in_data: PyAudio backend usage
+            :param frame_count: PyAudio backend usage
+            :param time_info: PyAudio backend usage
+            :param status: PyAudio backend usage
+            :return: frame of data, continue instruction for PyAudio
+            """
+            data = np.array(self.master.output_waveform(self.frequency)).astype(np.float32).tostring()
             return data, pyaudio.paContinue
 
+        # Create pyaudio stream
         stream = p.open(format=pyaudio.paFloat32,
                         channels=1,
                         rate=RATE,
@@ -46,7 +62,6 @@ class LoopWave(threading.Thread):
                         )
 
         if not self.end_now:
-
             stream.start_stream()
             while not self.end_now:
                 time.sleep(0.01)
@@ -57,8 +72,7 @@ class LoopWave(threading.Thread):
 
 class Synthesiser(tk.Tk):
     """
-    Subclass of Tk which adds widgets for synthesiser control.
-
+    Subclass of Tk, adds widgets for synthesiser control.
     :method setup_worker: Creates new LoopWave thread, assigns as class attribute
     :method start_sound: Run on button press, creates worker if one doesn't exist, starts wave sound loop
     :method stop_sound: Run on button press, stops wave sound loop, deletes worker so future worker can be created
@@ -66,87 +80,154 @@ class Synthesiser(tk.Tk):
     """
     def __init__(self):
         super().__init__()
+
+        # Create operator grid
         self.op_list = []
-        self.shape_list = [("sine", "sin", 0), ("square", "sqr", 1), ("sawtooth", "saw", 2), ("triangular", "tri", 3)]
+        for i in range(0,6):
+            self.op_list.append([])
+            for j in range(0,6):
+                self.op_list[i].insert(j, Operator(self))
+
+        # Create shape radiobuttons
+        self.shape_list = [("sine", "sin", 0), ("triangular", "tri", 1)]
         self.wave_shape = tk.StringVar()
         self.wave_shape.set("sin")
-        # Create sound control buttons
-        self.start_button = tk.Button(self, text="Play sound", padx=5, pady=5, command=(lambda: self.start_sound()))
-        self.start_button.grid(row=0, column=0)
-        self.stop_button = tk.Button(self, text="Stop sound", padx=5, pady=5, command=(lambda: self.stop_sound()))
-        self.stop_button.grid(row=1, column=0)
-        # Create plot wave button
-        self.plot_button = tk.Button(self, text="Plot wave", padx=5, pady=5, command=(lambda: self.plot_wave()))
-        self.plot_button.grid(row=2, column=0)
-        # Wave modulation section
-
-        self.add_op_button = tk.Button(self, text="Add operator", padx=5, pady=5, command=(lambda: self.add_op(self.frequency_slider.get(), self.volume_slider.get(), self.wave_shape.get(), phase=0)))
-        self.add_op_button.grid(row=0, column=8)
-        self.remove_op_button = tk.Button(self, text="Remove operator", padx=5, pady=5, command=(lambda: self.remove_op(self.op_listbox.curselection()[0])))
-        self.remove_op_button.grid(row=1, column=8)
-        self.modulate_op_button = tk.Button(self, text="Modulate operator", padx=5, pady=5, command=(lambda: self.modulate_op(self.op_list[self.op_listbox.curselection()[0]])))
-        self.modulate_op_button.grid(row=2, column=8)
-        self.op_listbox = tk.Listbox(self)
-        self.op_listbox.grid(row=0, column=9, rowspan=7, columnspan=2)
-
         for shape, shp, row in self.shape_list:
             tk.Radiobutton(self, text=shape, variable=self.wave_shape, value=shp).grid(row=row, column=2)
 
-        # Create sliders to control frequency, volume
-        self.frequency_slider = tk.Scale(self, from_=1, to_=500, label="Frequency", orient=tk.VERTICAL, length=200)
-        self.frequency_slider.grid(row=0,rowspan=5, column=3, columnspan=2)
-        self.frequency_slider.set(220)
-        self.volume_slider = tk.Scale(self, from_=1, to_=0, resolution=0.05, label="Volume", orient=tk.VERTICAL, length=200)
-        self.volume_slider.grid(row=0,rowspan=5,column=5,columnspan=2)
+        # Create sliders to control frequency, volume, modulator
+        self.frequency_slider = tk.Scale(self, from_=150, to_=0.1, resolution=0.1, bigincrement=1, label="Frequency",
+                                         orient=tk.VERTICAL, length=400)
+        self.frequency_slider.grid(row=0,rowspan=5, column=5, columnspan=2)
+        self.frequency_slider.set(10)
+        self.volume_slider = tk.Scale(self, from_=1, to_=0, resolution=0.05, label="Volume",
+                                      orient=tk.VERTICAL, length=400)
+        self.volume_slider.grid(row=0,rowspan=5,column=3,columnspan=2)
         self.volume_slider.set(1)
+        self.modulation_slider = tk.Scale(self, from_=10, to_=1, label="Modulation Index",
+                                          orient=tk.VERTICAL, length=400)
+        self.modulation_slider.grid(row=0, column=16, rowspan=5, columnspan=2)
+        self.modulation_slider.set(5)
 
-    def setup_worker(self):
+        # Modulator control canvas
+        self.mod_canvas = tk.Canvas(self, width=430, height=430, bg="white")
+        self.mod_canvas.grid(row=0, column=11, rowspan=10, columnspan=5)
+        for i in range(6):
+            for j in range(6):
+                tag = str(i) + "," + str(j)
+                self.mod_canvas.create_rectangle(((i + 1) * 10 + i * 60), ((j + 1) * 10 + j * 60),
+                                                 ((i + 1) * 70, ((j + 1) * 70)), fill="gray", tags=tag)
+                self.mod_canvas.tag_bind(tag, "<Button-1>", lambda event=None, q = tag: self.activate_op(q))
+                self.mod_canvas.tag_bind(tag, "<Button-3>", lambda event=None, q = tag: self.deactivate_op(q))
+                if j == 0:
+                    self.mod_canvas.itemconfig(tag, fill="white", activefill="light gray")
 
-        worker = LoopWave(self)
-        self.worker = worker
+        # Keyboard mapping to notes
+        self.bind("<KeyPress>", self.start_sound)
+        self.bind("<KeyRelease>", lambda event=None: self.stop_sound())
 
-    def start_sound(self):
-        if not hasattr(self, "worker") and self.op_listbox.curselection():
-            self.setup_worker()
+    def setup_worker(self, frequency):
+        """
+        Create new thread to play sound concurrently to GUI, creates class attribute "worker" for control
+        :param frequency: frequency of note to be played
+        :return: None
+        """
+        if not hasattr(self, "worker"):
+            worker = LoopWave(self, frequency)
+            self.worker = worker
+
+    def start_sound(self, event):
+        """
+        Create worker, start sound outpit
+        :param event: Key pressed, corresponds to dictionary frequency
+        :return: None
+        """
+        if not hasattr(self, "worker"):
+            self.setup_worker(NOTES[event.char.lower()])
             self.worker.start()
-        elif not self.op_listbox.curselection():
-            messagebox.showinfo("Error", "Please select an operator to play")
 
     def stop_sound(self):
+        """
+        Stop sound if worker exists
+        :return: None
+        """
         if hasattr(self, "worker"):
             self.worker.end_now = True
             del self.worker
 
     def stop_sound_exit(self):
+        """
+        Stop sound safely on exit window
+        :return: None
+        """
         if hasattr(self, "worker"):
             self.worker.end_now=True
             sys.exit()
         else:
             sys.exit()
 
-    def add_op(self, frequency, volume, shape, phase):
-        self.op_list.append(Operator(frequency, volume, shape, phase))
-        self.op_listbox.insert(tk.END, ("Freq=%sHz" % frequency, "Shape=%s" % shape))
+    def activate_op(self, tag):
+        """
+        Activate operator and set necessary attributes
+        :param tag: Corresponding square on canvas widget to operator
+        :return: None
+        """
+        i = int(tag[0])
+        j = int(tag[2])
+        if (j == 0 and not self.op_list[i][j].active) or self.op_list[i][j-1].active:
+            if j == 0:
+                self.op_list[i][j] = Operator(self, None, self.volume_slider.get(), self.wave_shape.get())
+                self.op_list[i][j].activate(tag)
+            elif j > 0 and not self.op_list[i][j].active:
+                self.op_list[i][j] = Operator(self, self.frequency_slider.get(), self.volume_slider.get(),
+                                              self.wave_shape.get())
+                self.op_list[i][j].activate(tag)
+                self.op_list[i][j-1].modulator = self.op_list[i][j]
+                self.op_list[i][j-1].mod_amp = self.modulation_slider.get()
+            elif j > 0 and self.op_list[i][j].active:
+                modamp = self.op_list[i][j].mod_amp
+                self.op_list[i][j] = Operator(self, self.frequency_slider.get(), self.volume_slider.get(),
+                                              self.wave_shape.get(), active=True)
+                self.op_list[i][j].modulator = self.op_list[i][j+1]
+                self.op_list[i][j - 1].modulator = self.op_list[i][j]
+                self.op_list[i][j].mod_amp = modamp
+        elif j == 0 and self.op_list[i][j].active:
+            modamp = self.op_list[i][j].mod_amp
+            self.op_list[i][j] = Operator(self, None, self.volume_slider.get(), self.wave_shape.get(), active=True)
+            self.op_list[i][j].modulator = self.op_list[i][j+1]
+            self.op_list[i][j].mod_amp = modamp
 
-    def remove_op(self, op_index):
-        del self.op_list[op_index]
-        self.op_listbox.delete(op_index)
+    def deactivate_op(self, tag):
+        """
+        Deactivate operator and remove necessary attributes
+        :param tag: Corresponding square on canvas widget to operator
+        :return: None
+        """
+        i = int(tag[0])
+        j = int(tag[2])
+        if self.op_list[i][j].active:
+            self.op_list[i][j].deactivate(tag)
+            self.op_list[i][j-1].modulator = np.zeros(CHUNK)
+            self.mod_canvas.itemconfig(tag, fill="white", activefill="light gray")
+            for k in range(j + 1, 6):
+                self.op_list[i][k] = Operator(self)
+                self.op_list[i][k].deactivate(tag[0:2] + str(k))
 
-    def modulate_op(self, carrier):
-        self.mod_index = int()
-        self.mod_amp = int()
-        mod = ModulatorSelect(self)
-        root.wait_window(mod)
-        carrier.modulator = self.op_list[self.mod_index]
-        carrier.mod_amp = self.mod_amp
-
-    def output_waveform(self):
-        outwave = self.op_list[self.op_listbox.curselection()[0]].waveform()
-        return outwave
-
-    def plot_wave(self):
-        plt.plot(np.concatenate((self.output_waveform(), self.output_waveform())))
-        plt.show()
+    def output_waveform(self, frequency):
+        """
+        Adds all active carrier waveforms together
+        :param frequency: Frequency of note for playback
+        :return: Numpy array containing audio data
+        """
+        self.op_list[0][0].frequency = frequency
+        outwave = self.op_list[0][0].waveform()
+        v = 1
+        for ops in self.op_list[1:]:
+            if ops[0].active:
+                ops[0].frequency = frequency
+                outwave = np.add(ops[0].waveform(), outwave)
+                v += 1
+        return self.volume_slider.get() * 1/v * outwave
 
 
 class Operator:
@@ -154,79 +235,82 @@ class Operator:
     Stores information required to generate smooth wave
 
     :method waveform: generates waveform based on class attributes
+
     """
-    def __init__(self, frequency, volume, shape, phase=0, modulator=np.zeros(CHUNK)):
+    def __init__(self, master, frequency=None, volume=1, shape=None, modulator=np.zeros(CHUNK), active=False):
+        """
+        Initialise class
+        :param master: Master tk window
+        :param frequency: Frequency of wave playback
+        :param volume: Volume of waveform
+        :param shape: Shape of waveform
+        :param modulator: Modulating operator, by default empty array
+        :param active: Wave activator
+        """
+        self.master = master
         self.frequency = frequency
         self.volume = volume
         self.shape = shape
-        self.phase = phase
-        self.t = np.linspace(0, CHUNK / RATE, int(CHUNK))
+        self.phase = 0
+        self.t = np.linspace(0, CHUNK / RATE, CHUNK)
         self.modulator = modulator
         self.mod_amp = 1
+        self.active = active
 
     def waveform(self):
         """
-        Generates wave as ndarray - frequency of datapoints dependent on rate
-
-        :return: Returns ndarray containing wave data
+        Generates wave as ndarray with properties depending on class attributes
+        :return: ndarray containing wave data
         """
         f = self.frequency
         if type(self.modulator) == np.ndarray:
             if self.shape == "sin":
                 sig = self.volume * (np.sin(2 * np.pi * (f * self.t + self.phase)))
-                self.phase = math.modf(f * CHUNK / RATE + self.phase)[0]
-            elif self.shape == "sqr":
-                sig = self.volume * np.array(signal.square(2 * np.pi * (f * self.t + self.phase)), dtype=np.float32)
-                self.phase = math.modf(f * CHUNK / RATE + self.phase)[0]
-            elif self.shape == "saw":
-                sig = self.volume * np.array(signal.sawtooth(2 * np.pi * (f * self.t + self.phase)), dtype=np.float32)
-                self.phase = math.modf(f * CHUNK / RATE + self.phase)[0]
+                self.phase = math.modf(f * (CHUNK + 1) / RATE + self.phase)[0]
             elif self.shape == "tri":
-                sig = self.volume * np.array(signal.sawtooth(2 * np.pi * (f * self.t + self.phase), width=0.5), dtype=np.float32)
-                self.phase = math.modf(f * CHUNK / RATE + self.phase)[0]
+                sig = self.volume * np.array(signal.sawtooth(2 * np.pi * (f * self.t + self.phase), width=0.5),
+                                             dtype=np.float32)
+                self.phase = math.modf(f * (CHUNK + 1) / RATE + self.phase)[0]
             else:
                 sig = np.zeros(CHUNK)
             return sig
         elif type(self.modulator) == Operator:
             if self.shape == "sin":
-                sig = self.volume * (np.sin(2 * np.pi * (f * self.t + self.phase) + self.mod_amp * self.modulator.waveform()))
-                self.phase = math.modf(f * CHUNK / RATE + self.phase)[0]
-            elif self.shape == "sqr":
-                sig = self.volume * np.array(signal.square(2 * np.pi * (f * self.t + self.phase) + self.mod_amp * self.modulator.waveform()), dtype=np.float32)
-                self.phase = math.modf(f * CHUNK / RATE + self.phase)[0]
-            elif self.shape == "saw":
-                sig = self.volume * np.array(signal.sawtooth(2 * np.pi * (f * self.t + self.phase) + self.mod_amp * self.modulator.waveform()), dtype=np.float32)
+                sig = self.volume * (np.sin(2 * np.pi * (f * self.t + self.phase) + self.mod_amp *
+                                            self.modulator.waveform()))
                 self.phase = math.modf(f * CHUNK / RATE + self.phase)[0]
             elif self.shape == "tri":
-                sig = self.volume * np.array(signal.sawtooth(2 * np.pi * (f * self.t + self.phase) + self.mod_amp * self.modulator.waveform(), width=0.5), dtype=np.float32)
+                sig = self.volume * np.array(signal.sawtooth(2 * np.pi * (f * self.t + self.phase) + self.mod_amp *
+                                                             self.modulator.waveform(), width=0.5), dtype=np.float32)
                 self.phase = math.modf(f * CHUNK / RATE + self.phase)[0]
             else:
                 sig = np.zeros(CHUNK)
             return sig
 
+    def activate(self, tag):
+        """
+        Activates operator corresponding to tag, sets square properties
+        :param tag: Corresponding square on canvas widget to operator
+        """
+        self.active = True
+        self.master.mod_canvas.itemconfig(tag, fill="yellow", activefill="#fcffa5")
+        active_next_tag = tag[0:2] + str(int(tag[2]) + 1)
+        self.master.mod_canvas.itemconfig(active_next_tag, fill="white", activefill="light gray")
 
-class ModulatorSelect(tk.Toplevel):
-    def __init__(self, master):
-        super().__init__()
-        self.master = master
-
-        self.op_box = tk.Listbox(self)
-        self.op_box.grid(row=0, column=0)
-        for op in self.master.op_list:
-            self.op_box.insert(tk.END, ("Freq=%sHz" % op.frequency, "Shape=%s" % op.shape))
-
-        self.ok_button = tk.Button(self, text="OK", padx=5, pady=5, command=(lambda: self.exit()))
-        self.ok_button.grid(row=1, column=0, rowspan=2)
-
-        self.amp_slider = tk.Scale(self, from_=20, to_=1, label="Modulation amplitude", orient=tk.VERTICAL, length=200)
-        self.amp_slider.grid(row=0,column=2)
-
-    def exit(self):
-        self.master.mod_index = self.op_box.curselection()[0]
-        self.master.mod_amp = self.amp_slider.get()
-        self.destroy()
+    def deactivate(self, tag):
+        """
+        Deactivates operator corresponding to tag, sets square properties
+        :param tag: Corresponding square on canvas widget to operator
+        """
+        if tag == "0,0":
+            self.master.stop_sound()
+        self.master.mod_canvas.itemconfig(tag, fill="gray", activefill="gray")
+        self.active = False
+        self.modulator = np.zeros(CHUNK)
+        self.shape = None
 
 
+# Create Synthesiser object
 root = Synthesiser()
 
 # Stop LoopAudio thread on window exit
